41a2d2da87ed00951aef2c7017073db694464cdb
zakaria <zikoxdodo@gmail.com>
2023-11-25 17:55:40 +0100

    start TASKIFY

---
+++ taskify/src/App.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <section className="section-container">
+      <h1 className="section-title">taskify</h1>
+    </section>
+  )
+}
+
+export default App
